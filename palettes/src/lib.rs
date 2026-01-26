38fde027da48582fb0934d7a0a7bd7055a60ec3d
David Thievon <david.thievon@proton.me>
2026-01-26 22:52:08 +0100

    fix(palettes): remove base16 themes in base24 themes
    Also add a test to ensure this doesn't happen again.

--- palettes/src/lib.rs
+++ palettes/src/lib.rs
@@ -10,3 +10,22 @@
 #![allow(clippy::too_many_lines)]
 
 include!(concat!(env!("OUT_DIR"), "/generated.rs"));
+
+#[cfg(test)]
+mod test {
+    use super::{Base16, Base24};
+    use palette_mapper::Palette;
+    use strum::IntoEnumIterator;
+
+    #[test]
+    /// Ensures base16 themes are actually 16 colors and base24 are actually 24 colors
+    fn ensure_proper_bases() {
+        for theme in Base16::iter() {
+            assert_eq!(Palette::from(theme).len(), 16);
+        }
+
+        for theme in Base24::iter() {
+            assert_eq!(Palette::from(theme).len(), 24);
+        }
+    }
+}
