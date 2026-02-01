4329f5097d553686e26163d4658ea7074ec329a7
David Thievon <david.thievon@proton.me>
2026-02-01 01:48:38 +0100

    test(e2e): create playwright-report dir if it doesn't exist
    Fixes ci failure.

--- web/e2e/tests/convert-image-base16.spec.js
+++ web/e2e/tests/convert-image-base16.spec.js
@@ -1,42 +1,42 @@
 import { expect, test } from "@playwright/test";
 
 test("convert image with pre-defined palette", async ({ page }, testInfo) => {
-    await page.goto("/");
+  await page.goto("/");
 
-    const algorithm = "ManhattanDistance";
+  const algorithm = "ManhattanDistance";
 
-    const select_algorithm = test.step("select algorithm", () => {
-        return page.locator("#algorithms").selectOption(algorithm);
-    });
-    await expect(await select_algorithm).toEqual([algorithm]);
+  const select_algorithm = test.step("select algorithm", () => {
+    return page.locator("#algorithms").selectOption(algorithm);
+  });
+  await expect(await select_algorithm).toEqual([algorithm]);
 
-    const palette = "base16::Everforest";
+  const palette = "base16::Everforest";
 
-    const select_palette = test.step("select palette", () => {
-        return page.locator("#base-palettes").selectOption(palette);
-    });
-    await expect(await select_palette).toEqual([palette]);
+  const select_palette = test.step("select palette", () => {
+    return page.locator("#base-palettes").selectOption(palette);
+  });
+  await expect(await select_palette).toEqual([palette]);
 
-    await test.step("upload image", async () => {
-        return await page
-            .getByText(/^Image$/)
-            .setInputFiles("test-imgs/diagonal_rgb_gradient_500x500.png");
-    });
+  await test.step("upload image", async () => {
+    return await page
+      .getByText(/^Image$/)
+      .setInputFiles("test-imgs/diagonal_rgb_gradient_500x500.png");
+  });
 
-    await test.step("convert", async () => {
-        return await page.locator("#submit").click();
-    });
+  await test.step("convert", async () => {
+    return await page.locator("#submit").click();
+  });
 
-    const img_src = await test.step("check output", async () => {
-        return await page.locator("#img_preview").getAttribute("src");
-    });
-    await expect(await img_src).toMatch(/blob:/);
+  const img_src = await test.step("check output", async () => {
+    return await page.locator("#img_preview").getAttribute("src");
+  });
+  await expect(await img_src).toMatch(/blob:/);
 
-    const sc = await page.screenshot();
-    await testInfo.attach("screenshot", {
-        body: sc,
-        contentType: "image/png",
-    });
+  const sc = await page.screenshot();
+  await testInfo.attach("screenshot", {
+    body: sc,
+    contentType: "image/png",
+  });
 
-    expect;
+  expect;
 });
